92a7f4a553f1848dc569064f452243f3a04794e4
박광주 <kjpar0317@innogrid.com>
2024-11-18 08:46:26 +0900

    update

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import WindiCSS from "vite-plugin-windicss";
 
 const path = require("path");
 const port = process.env.PORT || 8000;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), WindiCSS()],
+  plugins: [vue()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
